71558489ab5896ddb716b0eedaf86f95fb731db5
Jovan Paunovic <jovan.paunovic@elfak.rs>
2024-07-26 18:10:03 +0200

    frontend: dodata validacija za korisnicko ime pri registraciji

--- frontend/src/app/features/auth/components/register/register.component.ts
+++ frontend/src/app/features/auth/components/register/register.component.ts
@@ -15,12 +15,12 @@ import { createPasswordsMatchValidator } from '../../custom-form-validators/pass
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent implements OnInit {
-
   registerForm!: FormGroup;
   dataFromStore$!: Observable<any>;
 
-  constructor(private formBuilder: FormBuilder,
-              private store: Store<AppState>
+  constructor(
+    private formBuilder: FormBuilder,
+    private store: Store<AppState>
   ) {}
 
   ngOnInit(): void {
@@ -29,40 +29,44 @@ export class RegisterComponent implements OnInit {
   }
 
   initializeForm() {
-    this.registerForm = this.formBuilder.group({
-      firstName: [null, [Validators.required, Validators.minLength(3)]],
-      lastName: [null, [Validators.required, Validators.minLength(3)]],
-      username: [null, [Validators.required, Validators.minLength(3)]],
-      email: [null, [Validators.required, Validators.email]],
-      password: [
-        null,
-        [
-          Validators.required,
-          Validators.minLength(5),
-          createPasswordStrengthValidator()
+    this.registerForm = this.formBuilder.group(
+      {
+        firstName: [null, [Validators.required, Validators.minLength(3)]],
+        lastName: [null, [Validators.required, Validators.minLength(3)]],
+        username: [
+          null,
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.pattern('^[a-z0-9_.]+$'),
+          ],
         ],
-      ],
-      passwordConfirm: [
-        null,
-        [
-          Validators.required,
-          Validators.minLength(5)
+        email: [null, [Validators.required, Validators.email]],
+        password: [
+          null,
+          [
+            Validators.required,
+            Validators.minLength(5),
+            createPasswordStrengthValidator(),
+          ],
         ],
-      ],
-    }, {
-      validators: createPasswordsMatchValidator()
-    });
+        passwordConfirm: [null, [Validators.required, Validators.minLength(5)]],
+      },
+      {
+        validators: createPasswordsMatchValidator(),
+      }
+    );
   }
 
   selectDataFromStore() {
     this.dataFromStore$ = combineLatest({
       isSubmitting: this.store.select(authSelectors.selectIsSubmitting),
-      error: this.store.select(authSelectors.selectErrorMessage)
-    })
+      error: this.store.select(authSelectors.selectErrorMessage),
+    });
   }
 
   onRegisterFormSubmit() {
-    const registerData : RegisterRequest = this.registerForm.getRawValue();
-    this.store.dispatch(register({registerRequest: registerData}))
+    const registerData: RegisterRequest = this.registerForm.getRawValue();
+    this.store.dispatch(register({ registerRequest: registerData }));
   }
 }
